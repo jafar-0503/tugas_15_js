9404d1ac2db76f386e85ea0399ce38e4af5c990e
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-08-02 22:44:32 +0700

    tugas_15_js
    tugas sudha ok !!!

---
+++ tugas-15.js
@@ -0,0 +1,11 @@
+function sisisegitiga(){
+
+  var ab = 8;
+  var bc = 6;
+  var ac = Math.pow(ab,2) + Math.pow(bc,2);
+
+  console.log("Nilai Sisi AB:", ab,"cm");
+  console.log("Nilai Sisi BC:", bc, "cm");
+  console.log("Jadi, panjang sisi AC pada segitiga siku-siku tersebut adalah:", Math.sqrt(ac),"cm");
+}
+sisisegitiga();
